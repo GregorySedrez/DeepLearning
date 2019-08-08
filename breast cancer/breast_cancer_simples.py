1cfa97b7d97f18fe0e3bde26cc2abda25b2fb027
Gregory de Britto Sedrez <gregorysedrez@gmail.com>
2019-08-08 14:08:12 -0300

    seção4 aula 20

--- breast cancer/breast_cancer_simples.py
+++ breast cancer/breast_cancer_simples.py
@@ -1,35 +1,38 @@
 import pandas as pd
-import keras
-from.keras.models import Sequential
-from keras.layers import Dense
-from sklearn.model_selection import train_test_split
 
 previsores = pd.read_csv('entradas-breast.csv')
 classe = pd.read_csv('saidas-breast.csv')
 
-
+from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste,classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size = 0.25) 
 
-
+import keras
+from keras.models import Sequential #cria as camadas sequencialmente
+from keras.layers import Dense
 classificador = Sequential()
 
 #Configurando a rede neural
-classificador.add(Dense(units = 16, 
-			activation = "relu",
-			kernel_initializer = "random_uniform",
-			imput_dim = 30)
+classificador.add(Dense(units = 16, activation = 'relu',
+			kernel_initializer = 'random_uniform', input_dim = 30))
+#Adicionando mais uma camada na rede Neural
+#input_dim vai apaenas na primeira camada oculta
+#em geral utiliza-se a mesma quantidade de neurônios (16), porém, deve ser testada a melhor configuração
+classificador.add(Dense(units = 16, activation = 'relu',
+			kernel_initializer = 'random_uniform'))
 
-classificador.add(Dense(units = 1,
-			activation = "sigmoid")
+classificador.add(Dense(units = 1, activation = 'sigmoid'))
 
 #configuração e execução
-classificador.compile(	optimizer = "adam",
-			loss = "binary_crossentropy",
-			metrics = ["binary_accurancy"])
+
+otimizador = keras.optimizers.Adam(lr=0.001, decay = 0.0001, clipvalue = 0.5)
+classificador.compile(	optimizer = otimizador,loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+
+
+#classificador.compile(	optimizer = 'adam',loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
 
 classificador.fit(	previsores_treinamento,
-			classe_treinamento,
-			batch_size = 10
+		classe_treinamento,
+			batch_size = 10,
 			epochs = 100)
 
 #Previsão e Avaliação da Rede Neural
